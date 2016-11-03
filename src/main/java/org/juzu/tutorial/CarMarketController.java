678d7282a8f1050ea46445e4ab401d2480e44154
Ren <1342515499@qq.com>
2016-11-03 12:42:20 +0800

    updete test

--- src/main/java/org/juzu/tutorial/CarMarketController.java
+++ src/main/java/org/juzu/tutorial/CarMarketController.java
@@ -2,6 +2,6 @@ package org.juzu.tutorial;
 
 public class CarMarketController {
 	/*//test carMarketController
-	//test aaaaaaaaaaaaa
-	 * aaaaaaaaaaaaaa
+	//test bbbbbbbbbb
+	 * bbbbbbbbbbbbbbbbbbbbbbbb
 */}
