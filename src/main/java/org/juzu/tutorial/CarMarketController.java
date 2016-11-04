377fd39f16a983bbd618dc0a5681ea0fd21042fe
Ren <1342515499@qq.com>
2016-11-04 11:39:21 +0800

    eeeeeeeeeeeee

--- src/main/java/org/juzu/tutorial/CarMarketController.java
+++ src/main/java/org/juzu/tutorial/CarMarketController.java
@@ -1,5 +1,25 @@
 package org.juzu.tutorial;
 
+import javax.inject.Inject;
+
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+
+import net.wyun.qys.service.PolicyService;
+import net.wyun.qys.service.UserService;
+
 public class CarMarketController {
 
+	 private static final Log LOG = ExoLogger.getExoLogger(CarMarketController.class);
+		
+	  @Inject
+	  UserService userService;
+	  
+	  @Inject
+	  PolicyService policySvc;  
+	  
+	  
+	  
+	  
+	  
 }
