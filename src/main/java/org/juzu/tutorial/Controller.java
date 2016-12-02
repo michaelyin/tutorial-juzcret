a0a93a752aa423d70e3029eac74558a53ff4a65c
michaelyin <gt8442b@yahoo.com>
2016-12-02 16:18:03 -0500

    simplify class. move some codes to NationalPolicyController.

--- src/main/java/org/juzu/tutorial/Controller.java
+++ src/main/java/org/juzu/tutorial/Controller.java
@@ -24,11 +24,14 @@ import juzu.Response;
 import juzu.template.Template;
 import net.wyun.qys.domain.Policy;
 import net.wyun.qys.domain.UserSetting;
+import net.wyun.qys.domain.nationalpolicy.NPSourceType;
+import net.wyun.qys.domain.nationalpolicy.NPolicyType;
+import net.wyun.qys.domain.nationalpolicy.NationalPolicy;
 import net.wyun.qys.domain.standard.StanJcrFile;
 import net.wyun.qys.domain.standard.StanTag;
 import net.wyun.qys.domain.standard.Standard;
 import net.wyun.qys.domain.standard.StandardType;
-import net.wyun.qys.service.PolicyService;
+import net.wyun.qys.service.NationalPolicyService;
 import net.wyun.qys.service.StandardService;
 import net.wyun.qys.service.UserService;
 import net.wyun.qys.util.UserUtil;
@@ -72,180 +75,148 @@ import java.util.List;
 import java.util.Map;
 
 public class Controller {
-	
-  public Controller() {
+
+	public Controller() {
 		super();
 	}
 
-private static final Log LOG = ExoLogger.getExoLogger(Controller.class);
-  
-  @Inject
-  SearchService searchService;
-	
-  @Inject
-  UserService userService;
-  
-  @Inject
-  PolicyService policySvc;
-  
-  @Inject
-  StandardService standardSvc;
-  
- // @Inject
- // private IdentityManager identityManager;
+	private static final Log LOG = ExoLogger.getExoLogger(Controller.class);
 
-  @Inject
-  @Path("index.gtmpl")
-  Template index;
-  
-  @Inject
-  @Path("interpretation.gtmpl")
-  Template interpretation;
-  
-  @Inject
-  @Path("new_interpretation.gtmpl")
-  Template new_interpretation;
-  
-  @Inject
-  @Path("interpretation_upload.gtmpl")
-  Template interpretation_upload;
-  
-  @Assets({"interpretationcss", "interpretationjs"})  
-  @View
-  public Response.Content interpretation() throws IOException{
-	  return interpretation.ok();
-  }
-  
-  //interpretation.gtmpl中的上传页面,在map.gtmpl中也有一个update_data()方法
-  @Assets({"interpretation_uploadjs","interpretation_uploadcss"})
-  @View
-  public Response.Content update_data() throws IOException{
-	  return interpretation_upload.ok();
-  }
-  
-  @Assets({"new_interpretationjs","new_interpretationcss"})
-  @View
-  public Response.Content new_interpretation() throws IOException{
-	  return new_interpretation.ok();
-  }
+	@Inject
+	SearchService searchService;
 
-  @Assets({"indexcss", "indexjs"})
-  //@Assets("indexcss")
-  @View
-  public Response.Content index(SecurityContext securityContext) throws IOException {
-	  String username = securityContext.getRemoteUser();
-	  LOG.info("user: " + username);
-	  UserSetting setting = userService.getUserSetting(username);
-	  net.wyun.qys.model.User u = userService.loadUser(username);
-	  
-	  try{
-		  
-		  Identity identity = ConversationState.getCurrent().getIdentity();
-		  //org.exoplatform.social.core.identity.model.Identity identity = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, username, true);
-		  List<String> ms =UserUtil.getMemberships(identity);
-		  
-		  for (String m : ms) {
-			  LOG.info("membership: " + m);
-		  }
-	  }catch(Exception e){
-		  LOG.error(e);
-	  }
-	  
-	  
-	  if(setting != null){
-		  LOG.info("user setting: " + setting.getUsername());
-	  }
-	  if(u != null){
-		  LOG.info("user avatar: " + u.getAvatar());
-	  }
-	  
-	  //try policySvc
-	  LOG.info("save policy");
-	  Policy policy = new Policy();
-	  policy.setPolicyName("test db saving秦皇岛");
-	  policy.setStartDate(new Date());
-	  this.policySvc.save(policy);
-	  
-	  //LOG.info("search test begins: ");
-	  //searchTest();
-	 // LOG.info("search test ends. ");
-	  
-	 // this.jcrSearch();
-	  
-      return index.ok();
-  }
-  
-  private void saveStandard(){
-	  Standard s = new Standard();
-	  s.setName("国家标准2016");
-	  s.setCreateDate(new Date());
-	  s.setNum("001-2960-2016");
-	  s.setType(StandardType.专用车);
-	  s.setUuid("4eb65550-a36a-11e6-80f5-76304dec7eb7");
-	  
-	  StanJcrFile jFile = new StanJcrFile();
-	  jFile.setFileName("testfile.txt");
-	  jFile.setUploadDate(new Date());
-	  jFile.setUrl("temp/url");
-	  jFile.setUuid("4eb65550-a36a-11e6-0000-76304dec7eb7");
-	  
-	  s.addStanJcrFile(jFile);
-	  
-	  StanTag sTag1 = new StanTag();
-	  sTag1.setTag("first tag: car market 上海");
-	  s.addStanTag(sTag1);
-	  
-	  StanTag sTag = new StanTag();
-	  sTag.setTag("second tag: car market 北京");
-	  s.addStanTag(sTag);
-	  
-	  standardSvc.save(s);
-  }
-  
-  private void searchTest(){
-	  Collection<SearchResult> connectorResults = null;
-	  try {
-	      
-	      QysFileSearchServiceConnector fssc = new QysFileSearchServiceConnector(QysFileSearchServiceConnector.initFileSearchProp());
-	      fssc.setSearchSubDir("standard");
-	      connectorResults = fssc.searchQys("秦皇岛", "standard");
-	      
-	    } catch (Exception e) {
-	      LOG.error(e.getMessage(), e);
-	    }
-	    //return connectorResults;
-  }
-  
-  
-  
-  private void jcrSearch() {
-	  List<Node> ret = new ArrayList<Node>();
-	  
-	  SessionProvider sessionProvider = WCMCoreUtils.getUserSessionProvider();
-	   
-	  Session session;
-	try {
-		session = sessionProvider.getSession("collaboration", WCMCoreUtils.getRepository());
-		String docPath = "/Groups/spaces/exo_kai_fa_zu";
-		   
-		String q = "Select * from nt:file where jcr:path like " + docPath + "/%";
-		LOG.info("query string: " + q);
-		  NodeIterator iter = session.getWorkspace().getQueryManager().createQuery(q, Query.SQL).execute().getNodes();
-		   
-		  while (iter.hasNext()) {
-		   
-			  Node n = iter.nextNode();
-		      LOG.info("node: " + n.getPath());
-			  ret.add(n);
-		   
-		  }
-	} catch (RepositoryException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}  
-	  
-	   
-  }
-  
-  
+	@Inject
+	UserService userService;
+
+	@Inject
+	StandardService standardSvc;
+
+	@Inject
+	@Path("index.gtmpl")
+	Template index;
+
+
+	@Assets({ "indexcss", "indexjs" })
+	// @Assets("indexcss")
+	@View
+	public Response.Content index(SecurityContext securityContext) throws IOException {
+		String username = securityContext.getRemoteUser();
+		LOG.info("user: " + username);
+		UserSetting setting = userService.getUserSetting(username);
+		net.wyun.qys.model.User u = userService.loadUser(username);
+
+		try {
+
+			Identity identity = ConversationState.getCurrent().getIdentity();
+			// org.exoplatform.social.core.identity.model.Identity identity =
+			// identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME,
+			// username, true);
+			List<String> ms = UserUtil.getMemberships(identity);
+
+			for (String m : ms) {
+				LOG.info("membership: " + m);
+			}
+		} catch (Exception e) {
+			LOG.error(e);
+		}
+
+		if (setting != null) {
+			LOG.info("user setting: " + setting.getUsername());
+		}
+		if (u != null) {
+			LOG.info("user avatar: " + u.getAvatar());
+		}
+
+		// try policySvc
+		LOG.info("save policy");
+		NationalPolicy policy = new NationalPolicy();
+		policy.setName("test db saving秦皇岛");
+		policy.setNum("2016-national policy");
+		policy.setCreator(username);
+		policy.setDepartment("information");
+		policy.setSource(NPSourceType.发改委);
+		policy.setType(NPolicyType.保险);
+		policy.setCreateDate(new Date());
+		policy.setE_uuid("e_uuid");
+		policy.setT_uuid("t_uuid");
+		//this.policySvc.save(policy);
+
+		// LOG.info("search test begins: ");
+		// searchTest();
+		// LOG.info("search test ends. ");
+
+		// this.jcrSearch();
+
+		return index.ok();
+	}
+
+	private void saveStandard() {
+		Standard s = new Standard();
+		s.setName("国家标准2016");
+		s.setCreateDate(new Date());
+		s.setNum("001-2960-2016");
+		s.setType(StandardType.专用车);
+		s.setUuid("4eb65550-a36a-11e6-80f5-76304dec7eb7");
+
+		StanJcrFile jFile = new StanJcrFile();
+		jFile.setFileName("testfile.txt");
+		jFile.setUploadDate(new Date());
+		jFile.setUrl("temp/url");
+		jFile.setUuid("4eb65550-a36a-11e6-0000-76304dec7eb7");
+
+		s.addStanJcrFile(jFile);
+
+		StanTag sTag1 = new StanTag();
+		sTag1.setTag("first tag: car market 上海");
+		s.addStanTag(sTag1);
+
+		StanTag sTag = new StanTag();
+		sTag.setTag("second tag: car market 北京");
+		s.addStanTag(sTag);
+
+		standardSvc.save(s);
+	}
+
+	private void searchTest() {
+		Collection<SearchResult> connectorResults = null;
+		try {
+
+			QysFileSearchServiceConnector fssc = new QysFileSearchServiceConnector(
+					QysFileSearchServiceConnector.initFileSearchProp());
+			fssc.setSearchSubDir("standard");
+			connectorResults = fssc.searchQys("秦皇岛", "standard");
+
+		} catch (Exception e) {
+			LOG.error(e.getMessage(), e);
+		}
+		// return connectorResults;
+	}
+
+	private void jcrSearch() {
+		List<Node> ret = new ArrayList<Node>();
+
+		SessionProvider sessionProvider = WCMCoreUtils.getUserSessionProvider();
+
+		Session session;
+		try {
+			session = sessionProvider.getSession("collaboration", WCMCoreUtils.getRepository());
+			String docPath = "/Groups/spaces/exo_kai_fa_zu";
+
+			String q = "Select * from nt:file where jcr:path like " + docPath + "/%";
+			LOG.info("query string: " + q);
+			NodeIterator iter = session.getWorkspace().getQueryManager().createQuery(q, Query.SQL).execute().getNodes();
+
+			while (iter.hasNext()) {
+
+				Node n = iter.nextNode();
+				LOG.info("node: " + n.getPath());
+				ret.add(n);
+
+			}
+		} catch (RepositoryException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
